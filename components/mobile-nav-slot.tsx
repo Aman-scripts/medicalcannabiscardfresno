c1db0d4b839dacb4dfad129ebb419bf7b3f3554d
Amandeep <amandeep.nugen@gmail.com>
2026-08-18 15:05:30 +0530

    fixed

--- components/mobile-nav-slot.tsx
+++ components/mobile-nav-slot.tsx
@@ -3,6 +3,7 @@
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 
+
 const MobileNav = dynamic(
   () => import("@/components/mobile-nav").then((mod) => mod.MobileNav),
   { ssr: false },
